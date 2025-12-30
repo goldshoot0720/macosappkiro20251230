1bbe9696ae9499d74acbc3a574f3b1e878bf91af
goldshoot0720 <goldshoot0720@gmail.com>
2025-12-30 13:06:53 +0800

    Initial Commit

---
+++ macosappkiro20251230/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macosappkiro20251230
+//
+//  Created by 鋒兄 on 2025/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macosappkiro20251230/macosappkiro20251230App.swift
@@ -0,0 +1,17 @@
+//
+//  macosappkiro20251230App.swift
+//  macosappkiro20251230
+//
+//  Created by 鋒兄 on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct macosappkiro20251230App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
